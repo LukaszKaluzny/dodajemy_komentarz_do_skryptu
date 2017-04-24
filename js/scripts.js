264b39105fc7cc574820aad3d9f8d90380c26020
Lukasz Szczesniak <veiner87@gmail.com>
2017-04-24 13:58:52 +0200

    branch with master

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// plik scripts.js
+var name = prompt('Wpisz swoje imię');
+/*
+alert('Witaj, ' + name); - zakomentowane do sprawdzenia na później
+*/
+console.log('Witaj, ' + name);
+// komentarz liniowy
+/*
+komentarz
+blokowy
+*/
